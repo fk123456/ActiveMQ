e06e56cb19a2201e4acea1b5d2fa975f40a50b99
foreign <fangke@asiainfo.com>
2022-03-03 18:20:16 +0800

    生产者和消费者

---
+++ activemq01/src/main/java/com/foreign/Receiver.java
@@ -0,0 +1,40 @@
+package com.foreign;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @author fangke
+ * @Description: 消息接收者
+ * @Package
+ * @date: 2022/3/3 5:53 下午
+ * <p>
+ */
+public class Receiver {
+    public static void main(String[] args) throws Exception {
+        // 1. 建立工厂对象，
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(
+                ActiveMQConnectionFactory.DEFAULT_USER,
+                ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+                "tcp://172.16.13.147:61616"
+        );
+        //2 从工厂里拿一个连接
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        //3 从连接中获取Session(会话)
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 从会话中获取目的地(Destination)消费者会从这个目的地取消息
+        Queue queue = session.createQueue("user");
+
+        //从会话中创建消息提供者
+        MessageConsumer consumer = session.createConsumer(queue);
+
+        //从会话中创建文本消息(也可以创建其它类型的消息体)
+        while (true) {
+            TextMessage receive = (TextMessage)consumer.receive();
+            System.out.println("TextMessage:" + receive.getText());
+
+        }
+    }
+}
---
+++ activemq01/src/main/java/com/foreign/Sender.java
@@ -0,0 +1,46 @@
+package com.foreign;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @author fangke
+ * @Description: 消息发送者
+ * @Package
+ * @date: 2022/3/3 5:37 下午
+ * <p>
+ */
+public class Sender {
+    public static void main(String[] args) throws Exception {
+        // 1. 建立工厂对象，
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(
+                ActiveMQConnectionFactory.DEFAULT_USER,
+                ActiveMQConnectionFactory.DEFAULT_PASSWORD,
+                "tcp://172.16.13.147:61616"
+        );
+        //2 从工厂里拿一个连接
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+        //3 从连接中获取Session(会话)
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 从会话中获取目的地(Destination)，消费者会从这个目的地取消息
+        Queue queue = session.createQueue("user");
+
+
+        //从会话中创建消息提供者
+        MessageProducer producer = session.createProducer(queue);
+        //从会话中创建文本消息(也可以创建其它类型的消息体)
+
+        for (int i = 0; i < 100; i++) {
+            TextMessage message = session.createTextMessage("msg: " + i);
+            // 通过消息提供者发送消息到ActiveMQ
+            Thread.sleep(1000);
+            producer.send(message);
+        }
+
+        // 关闭连接
+        connection.close();
+        System.out.println("exit");
+    }
+}
